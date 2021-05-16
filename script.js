e506458fc486a178ad3fbae86f0941e86f226c59
Sam Knight <sknight3637@gmail.com>
2021-05-16 01:16:04 -0400

    finished

--- script.js
+++ script.js
@@ -1,25 +1,57 @@
 let varAmt;
-let container = document.querySelector(".container");
 let element;
-const button = document.querySelector("button");
+let color = '';
+const container = document.querySelector(".container");
+const redbtn = document.querySelector("#red");
+const bluebtn = document.querySelector("#blue");
+const greenbtn = document.querySelector("#green");
+const blackbtn = document.querySelector("#black");
+const whitebtn = document.querySelector("#white");
+
+redbtn.addEventListener('click', () => color = 'red');
+bluebtn.addEventListener('click', () => color = 'blue');
+greenbtn.addEventListener('click', () => color = 'green');
+blackbtn.addEventListener('click', () => color = 'black');
+whitebtn.addEventListener('click', () => color = 'white');
+
+
+const button = document.querySelector("#btn");
 
 function GetInputAndGrid() {
     do {
-        divAmt = parseInt(window.prompt("How many elements do you want?"), 10);
-    } while (isNaN(divAmt) || divAmt > 100 || divAmt < 0);
+        divAmt = parseInt(window.prompt("How many elements do you want? (1-64)"), 10);
+    } while (isNaN(divAmt) || divAmt > 64 || divAmt < 1);
+    container.style.gridTemplateColumns = `repeat(${divAmt}, 1fr)`;
     MakeGrid();
 }
 function MakeGrid () {
-    for (let i = 0; i < divAmt; i++)
+    for (let i = 0; i < divAmt * divAmt; i++)
     {
         element = document.createElement('div');
-        //element.textContent = 'Hello'
         container.appendChild(element);
-        element.style.background = 'red';
-        element.style.border = '10px solid red';
-        element.style.padding = '10px'
-        element.style.margin = '20px';
-        element.classList.add('div-hover');
+        element.classList = 'grid-element';
+        element.addEventListener('mouseover', ChangeColor);
+    }
+}
+function ChangeColor(e) {
+    switch(color) {
+        case 'red':
+            e.target.style.backgroundColor = 'red';
+            break;
+        case 'blue':
+            e.target.style.backgroundColor = 'blue';
+            break;
+        case 'green':
+            e.target.style.backgroundColor = 'green';
+            break;
+        case 'black':
+            e.target.style.backgroundColor = 'black';
+            break;
+        case 'white':
+            e.target.style.backgroundColor = 'white';
+            break;
+        default:
+            e.target.style.backgroundColor = 'black';
     }
 }
 function removeAllChildNodes(parent) {
